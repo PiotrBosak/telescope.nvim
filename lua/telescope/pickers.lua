25a1c6c0b30f587bd61d4cc0a70a7966a03083da
Piotr Bosak <pbk@touk.pl>
2022-10-21 09:09:48 +0200

    a

--- lua/telescope/pickers.lua
+++ lua/telescope/pickers.lua
@@ -1273,8 +1273,8 @@ function Picker:get_result_processor(find_id, prompt, status_updater)
     -- may need the prompt for tiebreak
     self.manager:add_entry(self, score, entry, prompt)
     status_updater { completed = false }
-    os.execute('tmux-windowizer tests abc')
     table.insert(my_list, entry)
+    os.execute([[echo 'a' >> /Users/pbk/test_nvim.txt]])
   end
 
   local cb_filter = function(_)
